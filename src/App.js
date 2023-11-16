37cc40f9b2a680b49354fe9162f847b4d3e4ba52
daniellberg <danielbergons@gmail.com>
2023-11-15 22:51:22 -0300

    feature: creates sum, minus, times and divided ffunctions

--- src/App.js
+++ src/App.js
@@ -7,39 +7,110 @@ import { useState } from 'react';
 const App = () => {
 
   const [currentNumber, setCurrentNumber] = useState('0');
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState('');
 
   const handleAddNumber = (number) => {
-    setCurrentNumber(prev => `${prev}${number}`);
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${number}`);
   }
 
+  const handleOnClear = () => {
+    setCurrentNumber('0');
+    setFirstNumber('0');
+  }
+
+  const handleSum = () => {
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('');
+      setOperation('+');
+    }else {
+      const sum = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber(String(sum));
+    }
+  }
+
+  const handleMinus = () => {
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('');
+      setOperation('-');
+    }else {
+      const total = Number(firstNumber) - Number(currentNumber);
+      setCurrentNumber(String(total));
+    }
+  }
+
+  const handleTimes = () => {
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('');
+      setOperation('*');
+    }else {
+      const total = Number(firstNumber) * Number(currentNumber);
+      setCurrentNumber(String(total));
+    }
+  }
+
+  const handleDivided = () => {
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('');
+      setOperation('/');
+    }else {
+      const total = Number(firstNumber) / Number(currentNumber);
+      setCurrentNumber(String(total));
+    }
+  }
+
+  const handleEquals = () => {
+    if(firstNumber !== '0' && operation !== '' && currentNumber !== 0){
+      switch(operation){
+        case '+':
+          handleSum();
+          break;
+        case '-':
+          handleMinus();
+          break;
+        case '*':
+          handleTimes();
+          break;
+        case '/':
+          handleDivided();
+          break;
+        default:
+          break
+      }
+    }
+  }
 
   return (
     <Container>
       <Content>
         <Input value={currentNumber}/>
         <Row>
-          <Button label="x" onClick={() => handleAddNumber('x')}/>
-          <Button label="/" onClick={() => handleAddNumber('/')}/>
-          <Button label="C" onClick={() => handleAddNumber('C')}/>
+          <Button label="x" onClick={handleTimes}/>
+          <Button label="/" onClick={handleDivided}/>
+          <Button label="C" onClick={handleOnClear}/>
           <Button label="%" onClick={() => handleAddNumber('%')}/>
         </Row>
         <Row>
           <Button label="7" onClick={() => handleAddNumber('7')}/>
           <Button label="8" onClick={() => handleAddNumber('8')}/>
           <Button label="9" onClick={() => handleAddNumber('9')}/>
-          <Button label="-" onClick={() => handleAddNumber('-')}/>
+          <Button label="-" onClick={handleMinus}/>
         </Row>
         <Row>
           <Button label="4" onClick={() => handleAddNumber('4')}/>
           <Button label="5" onClick={() => handleAddNumber('5')}/>
           <Button label="6" onClick={() => handleAddNumber('6')}/>
-          <Button label="+" onClick={() => handleAddNumber('+')}/>
+          <Button label="+" onClick={handleSum}/>
         </Row>
         <Row>
           <Button label="1" onClick={() => handleAddNumber('1')}/>
           <Button label="2" onClick={() => handleAddNumber('2')}/>
           <Button label="3" onClick={() => handleAddNumber('3')}/>
-          <Button label="=" onClick={() => handleAddNumber('=')}/>
+          <Button label="=" onClick={handleEquals}/>
         </Row>
       </Content>
     </Container>
