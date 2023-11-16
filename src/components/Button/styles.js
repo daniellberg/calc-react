ccf98070218c5baa997aa6c7b750a0022bbaaa06
daniellberg <danielbergons@gmail.com>
2023-11-15 21:45:14 -0300

    first commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #FAFAFA;
+    background-color: #000AF0;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.5;
+    }
+
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 85px;
+    background-color: #AAFAFA;
+    display: flex;
+    align-items: flex;
+    justify-content: flex-end;
+    
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    input{
+        width: 100%;
+        height: 85px;
+        background-color: #AAFAFA;
+        border: 0;
+        padding: 0 10px;
+
+        font-size: 24px;
+        font-family: 'Roboto';
+    }
+`
